988a4c521b6268ebbada29953eae1225f73428b9
Taisei <taisei.960420@gmail.com>
2025-07-25 07:39:05 +0900

    p95

---
+++ src/2/2.2/index.ts
@@ -0,0 +1,8 @@
+// 型注釈
+const greeting_two: string = "Hello, ";
+const target_two: string = "World!";
+console.log(greeting_two + target_two)
+
+// let greeting_three: string, target_three: string;
+// greeting_three = "Hello,";
+// console.log(greeting_three + target_three)
