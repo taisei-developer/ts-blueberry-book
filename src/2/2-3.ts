66b15ea484ab1ae1c09208c39b3a3b373d9e500f
Taisei <taisei.960420@gmail.com>
2025-07-27 17:13:52 +0900

    p103

--- src/2/2-3.ts
+++ src/2/2-3.ts
@@ -4,3 +4,14 @@ const small = 4e-5;
 
 console.log(big, small)
 // -> 100000000 0.00004
+
+// BigIntリテラル
+const bignum: bigint = (123n + 456n) * 2n
+
+console.log(bignum)
+// -> 1158n
+
+const result = 5n / 2n;
+
+console.log(result)
+// -> 2n
